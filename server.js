d0fba0fcd893c885f49730b44076882f67eab907
Kelvin Donkor <kwakuanor18@gmail.com>
2026-04-10 14:43:37 +0000

    feat: allow cross-origin requests from production frontend URL

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://quizlo.vercel.app"],
     credentials: true,
   }),
 );
